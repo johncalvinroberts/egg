f3dd29cef18211e2766f51f7eecca9bbf118c1fe
John Roberts <john.calvin.roberts@gmail.com>
2018-11-23 17:28:45 +0800

    remove _pages.scss; add parcel-out-of-box example

---
+++ examples/parcel-out-of-box/index.js
@@ -0,0 +1,6 @@
+import '../../dist/egg.min.css';
+
+var css =
+  'font-size: 40px; font-weight: 600; color: #333333; background-color: salmon;';
+
+console.log('%cHeck Yes %s', css, 'EGG CSS');
---
+++ vue-component-example.vue
@@ -0,0 +1,23 @@
+<style lang="scss" src="eggcss/components/modal.scss"></style>
+
+<template>
+  <div :class="['modal', activeClassName]">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    active: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+      activeClassName () {
+        return this.active ? 'active' : ''
+      }
+  }
+}
+</script>
